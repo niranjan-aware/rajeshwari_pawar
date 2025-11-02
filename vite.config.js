f862cf3e69c05e1ae35ed739b126d2bd8cbb89ba
Niranjan <Niranjan@dviodigital.com>
2025-11-02 20:24:42 +0530

    fix rollout issue

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          animations: ['framer-motion']
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'framer-motion']
+    emptyOutDir: true,
   }
 })
